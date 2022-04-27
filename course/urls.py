f4e0e3953628f6ab150e3df1ff060f30255fe771
Dino Wei <idreamer@gmail.com>
2022-04-27 11:07:51 +0800

    url change

--- course/urls.py
+++ course/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('course/', CourseList.as_view(), name='course_list'),
-    path('course/<int:cid>/', CourseView.as_view(), name='course_view'),
-    path('course/create/', CourseCreate.as_view(), name='course_create'),
+    path('', CourseList.as_view(), name='course_list'),
+    path('<int:cid>/', CourseView.as_view(), name='course_view'),
+    path('create/', CourseCreate.as_view(), name='course_create'),
 ]
